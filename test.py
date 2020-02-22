818b9d40cb94555fc7191af9ea588a90f8cdc26b
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2020-02-22 19:52:51 +0100

    generate proper tests, found bugs
    also test generation sometimes gets stuck

--- test.py
+++ test.py
@@ -3,10 +3,119 @@
 import sys
 from subprocess import Popen, PIPE
 from os.path import abspath, join as joinpath, dirname
-from random import randint
+from random import randint, choice, shuffle
 
-def eval(code):
-	ops = code.strip().split()
+UINT64_MAX = 0xffff_ffff_ffff_ffff
+
+def generate_game(min_size:int=1, max_size:int=7, max_number:int=UINT64_MAX, max_target:int=UINT64_MAX):
+	if min_size < 1:
+		raise ValueError('min_size has to be >= 1: %r' % min_size)
+
+	if max_size < min_size:
+		raise ValueError('max_size (%r) < min_size (%r)' % (max_size, min_size))
+
+	if max_size > 64:
+		raise ValueError('max_size too big: %r' % max_size)
+
+	if max_target > UINT64_MAX or max_target < 1:
+		raise ValueError('invalid max_target: %r' % max_target)
+
+	if max_number > UINT64_MAX or max_number < 1:
+		raise ValueError('invalid max_number: %r' % max_number)
+	
+	size = randint(min_size, max_size)
+	numbers = []
+	vals = []
+	code = []
+
+	def finished():
+		return (
+			len(numbers) == size and
+			len(vals) == 1 and
+			vals[0] > 0 and
+			vals[0] <= max_target
+		)
+
+	def generate_vals():
+		while len(numbers) < size:
+			if max_number <= 10 or randint(0, 1) == 0:
+				number = randint(1, 10)
+			else:
+				number = randint(11, max_number)
+
+			numbers.append(number)
+			vals.append(number)
+			code.append(number)
+
+			if finished(): return
+			generate_ops()
+			if finished(): return
+			generate_vals()
+			if finished(): return
+
+			code.pop()
+			vals.pop()
+			numbers.pop()
+
+	def generate_ops():
+		if len(vals) >= 2:
+			lhs = vals[-2]
+			rhs = vals.pop()
+
+			value = vals[-1] = lhs + rhs
+			code.append('+')
+			if finished(): return
+			generate_ops()
+			if finished(): return
+			generate_vals()
+			if finished(): return
+			code.pop()
+
+			if lhs >= rhs:
+				value = vals[-1] = lhs - rhs
+				code.append('-')
+				if finished(): return
+				generate_ops()
+				if finished(): return
+				generate_vals()
+				if finished(): return
+				code.pop()
+
+			value = vals[-1] = lhs * rhs
+			code.append('*')
+			if finished(): return
+			generate_ops()
+			if finished(): return
+			generate_vals()
+			if finished(): return
+			code.pop()
+
+			if lhs >= rhs and rhs != 0 and lhs % rhs == 0:
+				value = vals[-1] = lhs // rhs
+				code.append('/')
+				if finished(): return
+				generate_ops()
+				if finished(): return
+				generate_vals()
+				if finished(): return
+				code.pop()
+
+			vals.append(rhs)
+			vals[-2] = lhs
+
+	generate_vals()
+
+	if not finished():
+		raise RuntimeError(f'failed to generate game for parameters min_size={min_size}, max_size={max_size} (selected size={size}), max_number={max_number} max_target={max_target}')
+
+	return {
+		'target': vals[0],
+		'numbers': numbers,
+		'code': code,
+	}
+
+def eval(code:list) -> int:
+	ops = code
 	stack = []
 	for op in ops:
 		if op == '+':
@@ -32,10 +141,12 @@ binary_path = joinpath(dirname(abspath(__file__)), 'build', 'numbers')
 def test():
 	status = 0
 	fail_count = 0
-	succes_count = 0
-	for testnr in range(256):
-		target = randint(1, 999)
-		numbers = [randint(1, 200) for n in range(6)]
+	success_count = 0
+	failed_games = []
+	for testnr in range(1, 257):
+		game = generate_game(max_number=500, max_target=999)
+		target = game['target']
+		numbers = game['numbers']
 		pipe = Popen([binary_path, '--rpn', str(target), *[str(num) for num in numbers]], stdout=PIPE)
 
 		ok = True
@@ -44,31 +155,50 @@ def test():
 			if ok:
 				sys.stdout.write(' [ FAIL ]\n')
 				ok = False
-			sys.stdout.write(f'     {msg}\n')
+			sys.stdout.write(f'    {msg}\n')
 
-		sys.stdout.write(f'TEST: target={target}, numbers={repr(numbers)}'.ljust(73))
+		code = ' '.join(str(op) for op in game['code'])
+		sys.stdout.write(f'{testnr}: target={target}, numbers={repr(numbers)}, code={repr(code)}'.ljust(150))
+		sys.stdout.flush()
+		found_solution = False
 		for line in pipe.stdout:
 			line = line.decode().strip()
 			if line:
 				try:
-					output_target = eval(line)
+					output_target = eval(line.strip().split())
 				except (ValueError, IndexError):
 					write_fail(f'{line}: error evaluating code')
 				else:
-					if output_target != target:
+					if output_target == target:
+						found_solution = True
+					else:
 						write_fail(f'{line}: {output_target} != {target}')
 		code = pipe.wait()
 		if code != 0:
 			write_fail(f'exit code: {code}')
 
+		if not found_solution:
+			write_fail("No solution found!")
+			failed_games.append(game)
+
 		if ok:
 			print(' [  OK  ]')
-			succes_count += 1
+			success_count += 1
 		else:
 			status = 1
 			fail_count += 1
 
-	print(f'failed: {fail_count}, succeeded: {succes_count}')
+	if failed_games:
+		print()
+		print(f'Failed games:')
+		for game in failed_games:
+			target = game['target']
+			numbers = game['numbers']
+			code = ' '.join(str(op) for op in game['code'])
+			print(f'    target={target}, numbers={repr(numbers)}, code={repr(code)}')
+
+	print()
+	print(f'failed: {fail_count}, succeeded: {success_count}')
 
 	return status
 
