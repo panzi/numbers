74fe86b93eb8a4f38d2641132c6bdd94eefad849
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2020-03-23 03:16:08 +0100

    added license information

--- src/numbers.c
+++ src/numbers.c
@@ -1,3 +1,20 @@
+/**
+ *    numbers - a countdown numbers game solver
+ *    Copyright (C) 2020  Mathias Panzenböck
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -495,6 +512,10 @@ static void usage(int argc, char *const argv[]) {
 		"\t-e, --expr             Print solutions in usual notation (default).\n"
 		"\t-p, --paren            Like --expr but never skip parenthesis.\n"
 		"\n"
+		"numbers  Copyright (C) 2020  Mathias Panzenböck\n"
+		"This program comes with ABSOLUTELY NO WARRANTY.\n"
+		"This is free software, and you are welcome to redistribute it.\n"
+		"For more details see: https://github.com/panzi/numbers\n"
 	);
 }
 
